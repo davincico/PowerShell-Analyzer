1d0e4796f38c8b2c9288e8844fb73649720a0c07
Davin Hong <davin.hong3@gmail.com>
2024-03-22 01:39:58 +0800

    Added MD5 hash functionality
    Future considerations: Utilize hash and pull data from VirusTotal api

--- Powershell_Analyzer.py
+++ Powershell_Analyzer.py
@@ -5,16 +5,25 @@ try:
     import re
     import json
     import argparse
+    import hashlib
 except:
     print("Missing modules!")
     sys.exit(1)
+
+def get_md5(filetoinput):
+    fin = open(filetoinput, 'rb')
+    data = fin.read()
+    md5_results = hashlib.md5(data).hexdigest()
+    print(f"[*] MD5 hash of the file is: {md5_results}\n")
+    print(f"[*] Beginning PowerShell Analysis for {filetoinput}...\n\n")
+    print("PowerShell Script Indicators extracted split by categories:")
 
 def powershell_scanner(filetoinput):
     ps_indicators = json.load(open("ps_script_indicators.json"))
     fin = open(filetoinput, 'r')
     file = fin.read()
     for pattern in ps_indicators:
-        patternT = Table(title="PowerShell Script Indicators extracted split by categories")
+        patternT = Table()
         patternT.add_column(f"Suspicious indicators for {pattern}", justify="center")
 
 
@@ -37,6 +46,11 @@ if __name__ == "__main__":
     parser.add_argument("-f", "--file", required=True, help="powershell file to analyze")
     args = parser.parse_args()
     filetoinput = args.file
+
+    if not args.file:
+        print('error: must specify -f file')
+        sys.exit(1)
+    
     art = r"""
 __________                                 .__           .__  .__       _____                .__                              
 \______   \______  _  __ ___________  _____|  |__   ____ |  | |  |     /  _  \   ____ _____  |  | ___.__.________ ___________ 
@@ -47,4 +61,6 @@ __________                                 .__           .__  .__       _____   
     BY Davincico
     """
     print(art)
+    print("[*] Obtaining MD5 Hash of the file ...\n")
+    get_md5(filetoinput)
     powershell_scanner(filetoinput)
